0931f7fd78ce2ac9d126eb12ee50e5c1dc1a206c
sudheer426 <sudheer426@gmail.com>
2025-04-10 09:53:46 -0500

    modified host name

--- summercamp/settings.py
+++ summercamp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5kj5_)16g-^op_bcyh^ywytge@@s*h1^8uk2d+f#k5t@dw)hu!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '3.144.103.103']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '18.188.69.17']
 
 
 # Application definition
